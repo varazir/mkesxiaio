d43188a8d349a8570e0f062058c795f80e40d5a6
daniel.soderlund@gmail.com <daniel.soderlund@gmail.com@833906c6-0388-84b6-9b5e-a2244140942e>
2010-05-03 07:49:12 +0000

    base

---
+++ trunk/mkesxiaio_4.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#exec > >(tee vars1.log) 2>&1
+#set -x
+#set logs.log
+
+
+#	INFO
+#	All you now need is to download this file and the vmware ESXi ISO file
+#	sudo chmod +x mkesxiaio_x.x.sh
+#	sudo ./mkesxiaio_x.x.sh
+#
+#	You may need to download a extra file like inetd.conf
+#
+#	You need 2GB of free space to run the script 
+#
+#	Then you need a blank USB drive minimum 1GB OBS if this script
+#	breaks your USB drive or you loose DATA don't blame me.
+#	There will be warnings before anything happens to the USB drive.
+#
+#	I will test all installations types(script) on my Ubuntu system
+#
+#
+	
+
+####################Contact information####################
+#
+#	You can find me on Vmware's forum or www.vm-help.com/forum/index.php as Varazir
+#	E-mail varazir@gmail.com
+#		
