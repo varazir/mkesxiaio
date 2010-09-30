3567266ed21fbe3982f3dd20ea7478f88634e126
daniel.soderlund@gmail.com <daniel.soderlund@gmail.com@833906c6-0388-84b6-9b5e-a2244140942e>
2010-09-30 16:13:48 +0000

    small text update

--- new/mkesxiaio.sh
+++ new/mkesxiaio.sh
@@ -62,7 +62,7 @@ array_main_menu=(												#	Main menu (Array)
 ""																#1
 "   	Using"													#2	Iso file going to be used
 ""																#3
-"	1) ISO installation"										#4http://www.facebook.com/profile.php?id=742391901	To create a ISO file to burn on a CD for installation
+"	1) ISO installation"										#4	To create a ISO file to burn on a CD for installation
 "	2) USB installation"										#5	Creates custom made files that can be copied to a bootable USB drive for installation
 "	3) USB boot"												#6	Creates a custom DD file that can be written to a USB to boot and run ESXi
 "	4) USB installation without custom files"					#7	Copies the files from the ISO and make the USB bootable
