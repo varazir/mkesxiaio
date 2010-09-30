93bdd4838ab9060318ee4b857f55fdc1b6dc073b
daniel.soderlund@gmail.com <daniel.soderlund@gmail.com@833906c6-0388-84b6-9b5e-a2244140942e>
2010-09-30 17:33:19 +0000

    updated the INFO

--- new/mkesxiaio.sh
+++ new/mkesxiaio.sh
@@ -5,21 +5,21 @@
 
 
 #	INFO
-#	All you now need is to download this file and the vmware ESXi ISO file
+#	All you need is to download this file and the vmware ESXi ISO file
 #	sudo chmod +x mkesxiaio_x.x.sh
 #	sudo ./mkesxiaio_x.x.sh
-#
-#	You may need to download a extra file like inetd.conf
 #
 #	You need 2GB of free space to run the script 
 #
 #	Then you need a blank USB drive minimum 1GB OBS if this script
 #	breaks your USB drive or you loose DATA don't blame me.
-#	There will be warnings before anything happens to the USB drive.
+#	There will be warnings* before anything happens to the USB drive.
 #
 #	I will test all installations types(script) on my Ubuntu system
 #
 #
+#
+#	*If you don't run noninteracting 
 	
 
 ####################Contact information####################
